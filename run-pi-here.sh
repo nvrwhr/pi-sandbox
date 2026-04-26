0232425bba61f7de7a0f5a97e789f8d9ba9082fa
sp3c1 <bspecjalny@gmail.com>
2026-04-26 10:46:04 +0200

    cleanup at run-pi

--- run-pi-here.sh
+++ run-pi-here.sh
@@ -7,6 +7,6 @@ PROJECT_DIR="${1:-.}"
 # Resolve absolute project path
 ABS_PROJECT="$(pwd)/$PROJECT_DIR"
 
-echo "Running pi-sandbox:latest with >$ABS_PROJECT< mounted to /home/piuser/workspace ..."
+echo "Running pi-sandbox:latest with >$ABS_PROJECT< mounted to /root/workspace ..."
 
-docker run -it --rm -v ${ABS_PROJECT}:/home/piuser/workspace:rw -e PI_PROJECT_DIR=/home/piuser/workspace -w /home/piuser/workspace   pi-sandbox:latest
+docker run -it --rm -v ${ABS_PROJECT}:/root/workspace:rw -e PI_PROJECT_DIR=/root/workspace -w /root/workspace   pi-sandbox:latest
